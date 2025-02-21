89e50a9b325ac327645a521e0a5e79ad4e3b51bf
Kexagoon <45642990+kexagoon@users.noreply.github.com>
2025-02-21 12:11:53 +0100

    Delete scripts directory

--- scripts/animations.js
+++
@@ -1,9 +0,0 @@
-function animateAttack(player) {
-    // Здесь можно добавить логику для анимации атаки
-    console.log(`${player.name} attacks!`);
-}
-
-function animateDamage(player) {
-    // Анимация получения урона
-    console.log(`${player.name} takes damage!`);
-}
